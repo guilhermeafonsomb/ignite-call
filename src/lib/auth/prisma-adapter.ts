83dc95240d57283da2d556b6184cd76915882b11
Guilherme Afonso <guilherme.afonso@finderup.com.br>
2023-09-26 08:45:44 -0300

    create adapter prisma

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,11 +1,43 @@
 import { Adapter } from "next-auth/adapters";
 
+import { prisma } from "../prisma";
+
 export function PrismaAdapter(): Adapter {
   return {
     async createUser(user) {},
 
-    async getUser(id) {},
-    async getUserByEmail(email) {},
+    async getUser(id) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUniqueOrThrow({
+        where: {
+          email,
+        },
+      });
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
     async getUserByAccount({ providerAccountId, provider }) {},
     async updateUser(user) {},
     async deleteUser(userId) {},
